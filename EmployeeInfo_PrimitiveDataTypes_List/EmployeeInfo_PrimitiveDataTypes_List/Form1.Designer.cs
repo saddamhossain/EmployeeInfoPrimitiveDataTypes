8114ba6d56f7abd9c0f0e7c4b76cf8b0608d753e
saddamhossain <engg.rana.cse.seu@gmail.com>
2014-09-24 01:10:51 +0600

    Normal Employee Info Primitive Data Types

---
+++ EmployeeInfo_PrimitiveDataTypes_List/EmployeeInfo_PrimitiveDataTypes_List/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace EmployeeInfo_PrimitiveDataTypes_List
+{
+    partial class EmployeeInfoUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.employeeGroupBox = new System.Windows.Forms.GroupBox();
+            this.idLabel = new System.Windows.Forms.Label();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.emailLabel = new System.Windows.Forms.Label();
+            this.phoneLabel = new System.Windows.Forms.Label();
+            this.salaryLabel = new System.Windows.Forms.Label();
+            this.addressLabel = new System.Windows.Forms.Label();
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.phoneTextBox = new System.Windows.Forms.TextBox();
+            this.salaryTextBox = new System.Windows.Forms.TextBox();
+            this.addressTextBox = new System.Windows.Forms.TextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.employeeListView = new System.Windows.Forms.ListView();
+            this.idColumnHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.nameColumnHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.salaryColumnHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.employeeGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // employeeGroupBox
+            // 
+            this.employeeGroupBox.Controls.Add(this.saveButton);
+            this.employeeGroupBox.Controls.Add(this.addressTextBox);
+            this.employeeGroupBox.Controls.Add(this.salaryTextBox);
+            this.employeeGroupBox.Controls.Add(this.phoneTextBox);
+            this.employeeGroupBox.Controls.Add(this.emailTextBox);
+            this.employeeGroupBox.Controls.Add(this.nameTextBox);
+            this.employeeGroupBox.Controls.Add(this.idTextBox);
+            this.employeeGroupBox.Controls.Add(this.addressLabel);
+            this.employeeGroupBox.Controls.Add(this.salaryLabel);
+            this.employeeGroupBox.Controls.Add(this.phoneLabel);
+            this.employeeGroupBox.Controls.Add(this.emailLabel);
+            this.employeeGroupBox.Controls.Add(this.nameLabel);
+            this.employeeGroupBox.Controls.Add(this.idLabel);
+            this.employeeGroupBox.Location = new System.Drawing.Point(27, 12);
+            this.employeeGroupBox.Name = "employeeGroupBox";
+            this.employeeGroupBox.Size = new System.Drawing.Size(416, 290);
+            this.employeeGroupBox.TabIndex = 0;
+            this.employeeGroupBox.TabStop = false;
+            this.employeeGroupBox.Text = "Employee Entry";
+            // 
+            // idLabel
+            // 
+            this.idLabel.AutoSize = true;
+            this.idLabel.Location = new System.Drawing.Point(56, 38);
+            this.idLabel.Name = "idLabel";
+            this.idLabel.Size = new System.Drawing.Size(16, 13);
+            this.idLabel.TabIndex = 0;
+            this.idLabel.Text = "Id";
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(37, 70);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(35, 13);
+            this.nameLabel.TabIndex = 1;
+            this.nameLabel.Text = "Name";
+            // 
+            // emailLabel
+            // 
+            this.emailLabel.AutoSize = true;
+            this.emailLabel.Location = new System.Drawing.Point(40, 103);
+            this.emailLabel.Name = "emailLabel";
+            this.emailLabel.Size = new System.Drawing.Size(32, 13);
+            this.emailLabel.TabIndex = 2;
+            this.emailLabel.Text = "Email";
+            // 
+            // phoneLabel
+            // 
+            this.phoneLabel.AutoSize = true;
+            this.phoneLabel.Location = new System.Drawing.Point(34, 137);
+            this.phoneLabel.Name = "phoneLabel";
+            this.phoneLabel.Size = new System.Drawing.Size(38, 13);
+            this.phoneLabel.TabIndex = 3;
+            this.phoneLabel.Text = "Phone";
+            // 
+            // salaryLabel
+            // 
+            this.salaryLabel.AutoSize = true;
+            this.salaryLabel.Location = new System.Drawing.Point(36, 167);
+            this.salaryLabel.Name = "salaryLabel";
+            this.salaryLabel.Size = new System.Drawing.Size(36, 13);
+            this.salaryLabel.TabIndex = 4;
+            this.salaryLabel.Text = "Salary";
+            // 
+            // addressLabel
+            // 
+            this.addressLabel.AutoSize = true;
+            this.addressLabel.Location = new System.Drawing.Point(27, 200);
+            this.addressLabel.Name = "addressLabel";
+            this.addressLabel.Size = new System.Drawing.Size(45, 13);
+            this.addressLabel.TabIndex = 5;
+            this.addressLabel.Text = "Address";
+            // 
+            // idTextBox
+            // 
+            this.idTextBox.Location = new System.Drawing.Point(110, 35);
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.Size = new System.Drawing.Size(261, 20);
+            this.idTextBox.TabIndex = 1;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(110, 70);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(261, 20);
+            this.nameTextBox.TabIndex = 6;
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(110, 103);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(261, 20);
+            this.emailTextBox.TabIndex = 7;
+            // 
+            // phoneTextBox
+            // 
+            this.phoneTextBox.Location = new System.Drawing.Point(110, 137);
+            this.phoneTextBox.Name = "phoneTextBox";
+            this.phoneTextBox.Size = new System.Drawing.Size(261, 20);
+            this.phoneTextBox.TabIndex = 8;
+            // 
+            // salaryTextBox
+            // 
+            this.salaryTextBox.Location = new System.Drawing.Point(110, 167);
+            this.salaryTextBox.Name = "salaryTextBox";
+            this.salaryTextBox.Size = new System.Drawing.Size(261, 20);
+            this.salaryTextBox.TabIndex = 9;
+            // 
+            // addressTextBox
+            // 
+            this.addressTextBox.Location = new System.Drawing.Point(110, 200);
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(261, 20);
+            this.addressTextBox.TabIndex = 10;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(287, 240);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 11;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // employeeListView
+            // 
+            this.employeeListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.idColumnHeader,
+            this.nameColumnHeader,
+            this.salaryColumnHeader});
+            this.employeeListView.FullRowSelect = true;
+            this.employeeListView.GridLines = true;
+            this.employeeListView.Location = new System.Drawing.Point(27, 308);
+            this.employeeListView.Name = "employeeListView";
+            this.employeeListView.Size = new System.Drawing.Size(416, 139);
+            this.employeeListView.TabIndex = 1;
+            this.employeeListView.UseCompatibleStateImageBehavior = false;
+            this.employeeListView.View = System.Windows.Forms.View.Details;
+            this.employeeListView.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.employeeListView_MouseDoubleClick);
+            // 
+            // idColumnHeader
+            // 
+            this.idColumnHeader.Text = "Id";
+            this.idColumnHeader.Width = 78;
+            // 
+            // nameColumnHeader
+            // 
+            this.nameColumnHeader.Text = "Name";
+            this.nameColumnHeader.Width = 98;
+            // 
+            // salaryColumnHeader
+            // 
+            this.salaryColumnHeader.Text = "Salary";
+            this.salaryColumnHeader.Width = 232;
+            // 
+            // EmployeeInfoUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 488);
+            this.Controls.Add(this.employeeListView);
+            this.Controls.Add(this.employeeGroupBox);
+            this.MaximizeBox = false;
+            this.Name = "EmployeeInfoUI";
+            this.Text = "Employee Information Software";
+            this.Load += new System.EventHandler(this.EmployeeInformationUI_Load);
+            this.employeeGroupBox.ResumeLayout(false);
+            this.employeeGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox employeeGroupBox;
+        private System.Windows.Forms.Label addressLabel;
+        private System.Windows.Forms.Label salaryLabel;
+        private System.Windows.Forms.Label phoneLabel;
+        private System.Windows.Forms.Label emailLabel;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label idLabel;
+        private System.Windows.Forms.TextBox addressTextBox;
+        private System.Windows.Forms.TextBox salaryTextBox;
+        private System.Windows.Forms.TextBox phoneTextBox;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox idTextBox;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.ListView employeeListView;
+        private System.Windows.Forms.ColumnHeader idColumnHeader;
+        private System.Windows.Forms.ColumnHeader nameColumnHeader;
+        private System.Windows.Forms.ColumnHeader salaryColumnHeader;
+    }
+}
+
---
+++ EmployeeInfo_PrimitiveDataTypes_List/EmployeeInfo_PrimitiveDataTypes_List/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmployeeInfo_PrimitiveDataTypes_List
+{
+    public partial class EmployeeInfoUI : Form
+    {
+        public EmployeeInfoUI()
+        {
+            InitializeComponent();
+        }
+
+        List<int> idList = new List<int>();
+        List<string> nameList = new List<string>();
+        List<string> emailList = new List<string>();
+        List<string> phoneList = new List<string>();
+        List<int> salaryList = new List<int>();
+        List<string> addressList = new List<string>(); 
+
+        private void EmployeeInformationUI_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(idTextBox.Text);
+            string name = nameTextBox.Text;
+            string email = emailTextBox.Text;
+            string phone = phoneTextBox.Text;
+            int salary = Convert.ToInt32(salaryTextBox.Text);
+            string address = addressTextBox.Text;
+
+            idList.Add(id);
+            nameList.Add(name);
+            emailList.Add(email);
+            phoneList.Add(phone);
+            salaryList.Add(salary);
+            addressList.Add(address);
+
+            idTextBox.Text = "";
+            nameTextBox.Text = "";
+            emailTextBox.Text = "";
+            phoneTextBox.Text = "";
+            salaryTextBox.Text = "";
+            addressTextBox.Text = "";
+
+            ListViewItem items = new ListViewItem(id.ToString());
+            items.SubItems.Add(name);
+            items.SubItems.Add(email);
+            items.SubItems.Add(phone);
+            items.SubItems.Add(salary.ToString());
+            items.SubItems.Add(address);
+
+            employeeListView.Items.Add(items);
+
+        }
+
+        private void employeeListView_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            int selectedIndex = employeeListView.SelectedIndices[0];
+
+            idTextBox.Text = idList[selectedIndex].ToString();
+            nameTextBox.Text = nameList[selectedIndex];
+            emailTextBox.Text = emailList[selectedIndex];
+            phoneTextBox.Text = phoneList[selectedIndex];
+            salaryTextBox.Text = salaryList[selectedIndex].ToString();
+            addressTextBox.Text = addressList[selectedIndex];
+
+
+
+        }
+    }
+}
